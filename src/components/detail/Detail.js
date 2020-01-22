ed10e10155a1e48a37633bb5fb872b11e26c92c5
Thiago Diniz <thiagoodiniz@hotmail.com>
2020-01-22 14:53:25 -0300

    change table size and font-size

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -51,7 +51,7 @@ class Detail extends Component {
 						</div>
 					</header>
 
-					<Table pagination={false} dataSource={this.state.data} size="middle" scroll={{ x: '90%' }} >
+					<Table pagination={false} dataSource={this.state.data} size="small" scroll={{ x: '90%' }} >
 						{Object.keys(this.possibleItems[this.cardType].details).map((item) => 
 							<Column align="center" title={this.possibleItems[this.cardType].details[item]} dataIndex={item} key={item} />
 						)}
